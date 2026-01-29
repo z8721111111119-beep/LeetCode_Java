a7669540bd0f1b2e09ddd1a26bcf5db44abe70fa
James <z8721111111119@gmail.com>
2026-01-29 18:36:11 +0800

    第一次使用git測試

---
+++ twoSum/brute_force/TwoSum.java
@@ -0,0 +1,31 @@
+package twoSum.brute_force;
+
+class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		int num1, num2;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				num1 = nums[i];
+				num2 = nums[j];
+				if (num1 + num2 == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		return new int[] {};
+	}
+}
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		Solution s1 = new Solution();
+		int[] nums = new int[] { 2, 7, 11, 15 };
+		int target = 9;
+		int[] res = s1.twoSum(nums, target);
+		for (int i : res) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
