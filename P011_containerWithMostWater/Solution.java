46b9ef20dc0ffae2335ba1879213ae455cc4d04a
James <z8721111111119@gmail.com>
2026-07-31 10:35:26 +0800

    重構P011_containerWithMostWate

--- P011_containerWithMostWater/Solution.java
+++ P011_containerWithMostWater/Solution.java
@@ -1,24 +1,64 @@
 package P011_containerWithMostWater;
 
+/*
+Find two lines that together with the x-axis form a container, 
+such that the container contains the most water.
+Return the maximum amount of water a container can store.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+*/
+
 public class Solution {
-    public int maxArea(int[] height) {
-    	int left = 0,right=height.length-1,h,w,area,maxArea=0;
-    	while(right>left) {
-    		System.out.println("left="+left+",right="+right);
-    		h=Math.min(height[left], height[right]);
-    		w=right-left;
-    		area=h*w;
-    		if(height[left]>height[right])right--;
-    		if(height[left]<=height[right])left++;
-    		if (area>maxArea)maxArea=area;
-    	}
-    	return maxArea;
-	}
-	
-	
 	public static void main(String[] args) {
-		int[] height = new int[] {1,8,6,2,5,4,8,3,7};
-		Solution s1=new Solution();
-		System.out.print(s1.maxArea(height));
+		int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+		System.out.println(S1.maxArea(height));
 	}
 }
+
+class S1 {
+	public static int maxArea(int[] height) {
+		int left = 0;
+		int right = height.length - 1;
+		int maxArea = 0;
+
+		while (right > left) {
+			int h = Math.min(height[right], height[left]);
+			int area = h * (right - left);
+			maxArea = Math.max(maxArea, area);
+
+			if (height[right] > height[left]) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return maxArea;
+	}
+}
+
+// class S1 {
+// public static int maxArea(int[] height) {
+// int left = 0;
+// int right = height.length - 1;
+// int h = 1;
+// int area = 1;
+// int maxArea = 0;
+
+// while (right > left) {
+// if (height[right] > height[left]) {
+// h = height[left];
+// area = h * (right - left);
+// left++;
+// } else {
+// h = height[right];
+// area = h * (right - left);
+// right--;
+// }
+// if (maxArea < area) {
+// maxArea = area;
+// }
+// }
+// return maxArea;
+// }
+// }
